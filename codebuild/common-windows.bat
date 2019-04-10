05dd36cc678d021bfd42006744572b86b8309139
Justin Boswell <boswej@amazon.com>
2019-04-10 14:24:01 -0700

    try installing directly from a downloaded installer for dotnet

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,8 +1,10 @@
 
 :: install chocolatey
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+::"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/3c43f486-2799-4454-851c-fa7a9fb73633/673099a9fe6f1cac62dd68da37ddbc1a/dotnet-sdk-2.2.203-win-x64.exe -OutFile %TEMP%\dotnet-sdk-install.exe"
+%TEMP%\dotnet-sdk-install.exe /silent
 :: install dotnet
-choco install dotnetcore-sdk
+choco install dotnetcore-sdk -y
 
 dotnet test tests || goto :error
 
