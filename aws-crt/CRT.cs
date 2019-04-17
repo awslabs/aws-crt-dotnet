f83634ffe9055a8f2843ce1f20c98d9a1c2154bc
Justin Boswell <boswej@amazon.com>
2019-04-17 16:24:23 -0700

    double dammit

--- aws-crt/CRT.cs
+++ aws-crt/CRT.cs
@@ -158,9 +158,9 @@ namespace Aws.Crt
             public override LibraryHandle LoadLibrary(string name)
             {
                 Assembly crtAsm = Assembly.GetAssembly(typeof(CRT));
-                Console.WriteLine("ASM LOCATION: {1}", crtAsm.Location);
+                Console.WriteLine("ASM LOCATION: {0}", crtAsm.Location);
                 string path = crtAsm.Location.Replace(crtAsm.GetName().Name + ".dll", name);
-                Console.WriteLine("DLL LOCATION: {1}", path);
+                Console.WriteLine("DLL LOCATION: {0}", path);
                 return new LibraryHandle(kernel32.LoadLibrary(path));
             }
 
