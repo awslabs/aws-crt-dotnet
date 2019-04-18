e2ce13a770fd9d06c5b082928716841e9f15bb30
Justin Boswell <boswej@amazon.com>
2019-04-18 09:33:10 -0700

    try release config

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,7 +1,7 @@
 
 call RefreshEnv.Cmd
 
-dotnet test tests -v normal %*
+dotnet test tests --configuration Release -v normal %*
 %TEMP%\depends\depends.exe /c /ot:%TEMP%\depends.log tests/bin/Debug/netcoreapp2.1/aws-crt-dotnet.dll
 type %TEMP%\depends.log
 
