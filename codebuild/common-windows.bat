9caf86263fbd2a9fb09685bf5ddbd566e660c7cd
Justin Boswell <boswej@amazon.com>
2019-04-18 09:49:16 -0700

    dammit

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -2,7 +2,7 @@
 call RefreshEnv.Cmd
 
 dotnet test tests --configuration Release -v normal %*
-%TEMP%\depends\depends.exe /c /ot:%TEMP%\depends.log tests/bin/Debug/netcoreapp2.1/aws-crt-dotnet.dll
+%TEMP%\depends\depends.exe /c /ot:%TEMP%\depends.log tests/bin/Release/netcoreapp2.1/aws-crt-dotnet.dll
 type %TEMP%\depends.log
 
 goto :EOF
