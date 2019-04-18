5bfddad0e84503f23fd48b531806ea1ee7c6fb7d
Justin Boswell <boswej@amazon.com>
2019-04-17 17:58:07 -0700

    Fixed path/configuration for dll

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,11 +1,9 @@
 
 call RefreshEnv.Cmd
 
-dotnet build tests --configuration=RelWithDebInfo %* || goto :error
+dotnet test tests -v normal %* || goto :error
 %TEMP%\depends\depends.exe /c /ot:%TEMP%\depends.log tests/bin/Debug/netcoreapp2.1/aws-crt-dotnet.dll
 type %TEMP%\depends.log
-
-dotnet test tests -v normal --configuration=RelWithDebInfo %* || goto :error
 
 goto :EOF
 
