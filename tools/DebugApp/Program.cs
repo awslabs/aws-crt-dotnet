6803a6d9b72aa80501493b0cb1d6a46d4ddda7a1
Justin Boswell <boswej@amazon.com>
2019-04-22 18:14:10 -0700

    updated DebugApp to test Http

--- tools/DebugApp/Program.cs
+++ tools/DebugApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using Aws.Crt.IO;
+using Aws.Crt.Http;
 
 // Any code you put in Main() can be debugged natively by running
 // dotnet exec /path/to/DebugApp/bin/Debug/netcoreapp2.1/DebugApp.dll
@@ -11,6 +12,21 @@ namespace DebugApp
         static void Main(string[] args)
         {
             Console.WriteLine("HELLO WORLD");
+            var elg = new EventLoopGroup(1);
+            var clientBootstrap = new ClientBootstrap(elg);
+            var options = new HttpClientConnectionOptions();
+            options.ClientBootstrap = clientBootstrap;
+            options.HostName = "www.amazon.com";
+            options.Port = 80;
+            options.OnConnectionSetup = (int errorCode) =>
+            {
+                Console.WriteLine("CONNECTED");
+            };
+            options.OnConnectionShutdown = (int errorCode) =>
+            {
+                Console.WriteLine("DISCONNECTED");
+            };
+            var connection = new HttpClientConnection(options);
         }
     }
 }
