78e7568f9b212eba816a51af8cef10b35f67988a
Justin Boswell <boswej@amazon.com>
2019-05-02 19:01:45 -0700

    Use aws-c-http integration tests script

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -4,6 +4,14 @@ set -ex
 
 dotnet test -v normal
 
+if [ "$AWS_DOTNET_RUNTIME" == "" ]; then
+    exit 0
+fi
+
+dotnet publish --self-contained --runtime $AWS_DOTNET_RUNTIME tools/Elasticurl
+
+python3 build/deps/build/src/AwsCHttp/integration-testing/http_client_test.py tools/Elasticurl/bin/Debug/netcoreapp2.2/$AWS_DOTNET_RUNTIME/publish/Elasticurl.NET
+
 dotnet run --project tools/Elasticurl -- -v ERROR -P -H "content-type: application/json" -i -d '{"test": "testval"}' http://httpbin.org/post
 dotnet run --project tools/Elasticurl -- -v ERROR -i https://example.com
 
