87fd8e02b3a90cc4a9a582d0f9924288494684c0
Justin Boswell <boswej@amazon.com>
2019-05-03 08:56:07 -0700

    removed redundant tests

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -11,7 +11,3 @@ fi
 dotnet publish --self-contained --runtime $AWS_DOTNET_RUNTIME tools/Elasticurl
 
 python3 build/deps/build/src/AwsCHttp/integration-testing/http_client_test.py tools/Elasticurl/bin/Debug/netcoreapp2.2/$AWS_DOTNET_RUNTIME/publish/Elasticurl.NET
-
-dotnet run --project tools/Elasticurl -- -v ERROR -P -H "content-type: application/json" -i -d '{"test": "testval"}' http://httpbin.org/post
-dotnet run --project tools/Elasticurl -- -v ERROR -i https://example.com
-
