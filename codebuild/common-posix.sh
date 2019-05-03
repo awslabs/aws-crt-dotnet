dbc6de8ae9ce483a1bff117b6c20a303b5c8de89
Justin Boswell <boswej@amazon.com>
2019-05-02 18:33:15 -0700

    removed useless HttpClientConnectionTest, added curl tests to posix CI

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 dotnet test -v normal
+
+dotnet run --project tools/Elasticurl -- -v ERROR -P -H "content-type: application/json" -i -d '{"test": "testval"}' http://httpbin.org/post
+dotnet run --project tools/Elasticurl -- -v ERROR -i https://example.com
+
