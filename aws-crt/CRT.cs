3d8413d914500349b8e398259c2f59eecea7edf6
Justin Boswell <boswej@amazon.com>
2019-05-15 10:36:06 -0700

    force scope on extracted library file stream

--- aws-crt/CRT.cs
+++ aws-crt/CRT.cs
@@ -148,9 +148,11 @@ namespace Aws.Crt
                     string prefix = Path.GetRandomFileName();
                     var extractedLibraryPath = Path.GetTempPath() + prefix + "." + libraryName;
                     // Open the shared lib stream, write the embedded stream to it, and it will be deleted after the library is loaded
-                    var libStream = new FileStream(extractedLibraryPath, FileMode.Create, FileAccess.Write, FileShare.Read, 4096, FileOptions.None);
-                    resourceStream.CopyTo(libStream);
-                    return libStream.Name;
+                    using (var libStream = new FileStream(extractedLibraryPath, FileMode.Create, FileAccess.Write, FileShare.Read, 4096, FileOptions.None))
+                    {
+                        resourceStream.CopyTo(libStream);
+                        return extractedLibraryPath;
+                    }
                 }
             }
 
