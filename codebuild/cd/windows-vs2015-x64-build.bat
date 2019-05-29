6d1480e6dadc722e8d48ab36b95f6b372081a5ec
Justin Boswell <boswej@amazon.com>
2019-05-29 16:43:16 -0700

    stole the CD scripts from aws-crt-java and prepped a first draft for .NET

---
+++ codebuild/cd/windows-vs2015-x64-build.bat
@@ -0,0 +1,18 @@
+
+@setlocal enableextensions enabledelayedexpansion
+
+dotnet build -c Release -p:CMakeGenerator="Visual Studio 14 2015 Win64" || goto error
+
+for /f %%A in ('git describe --tags') do (
+    set GIT_TAG=%%A
+)
+
+aws s3 cp --recursive --exclude "*" --include "*.dll" .\build\lib s3://aws-crt-java-pipeline/%GIT_TAG%/x64
+
+@endlocal
+goto :EOF
+
+:error
+@endlocal
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
