224de0be0f5b5bd5c5ff432dec84dff315da435d
Justin Boswell <boswej@amazon.com>
2019-05-29 18:19:42 -0700

    curl the correct nuget url to query existence of a version of a package

--- codebuild/cd/test-version-exists.sh
+++ codebuild/cd/test-version-exists.sh
@@ -3,17 +3,17 @@ set -ex
 # force a failure if there's no tag
 git describe --tags
 # now get the tag
-CURRENT_TAG=$(git describe --tags)
+CURRENT_TAG=$(git describe --tags | cut -f2 -dv)
 # convert v0.2.12-2-g50254a9 to 0.2.12
-CURRENT_TAG_VERSION=$(git describe --tags | cut -f1 -d'-')
+CURRENT_TAG_VERSION=$(git describe --tags | cut -f1 -d'-' | cut -f2 -dv)
 # if there's a hash on the tag, then this is not a release tagged commit
 if [ "$CURRENT_TAG" != "$CURRENT_TAG_VERSION" ]; then
     echo "Current tag version is not a release tag, cut a new release if you want to publish."
     exit 1
 fi
-curl -sL "https://www.nuget.org/packages/AWSCRT/${CURRENT_TAG_VERSION}" -o /tmp/AWSCRT.${CURRENT_TAG_VERSION}.nupkg
-curl -sL "https://www.nuget.org/packages/AWSCRT-HTTP/${CURRENT_TAG_VERSION}" -o /tmp/AWSCRT-HTTP.${CURRENT_TAG_VERSION}.nupkg
-if [ -e /tmp/AWSCRT.${CURRENT_TAG_VERSION}.nupkg ] &&  [ -e /tmp/AWSCRT-HTTP.${CURRENT_TAG_VERSION}.nupkg ]; then
+AWSCRT_STATUS=$(curl -IsL https://api.nuget.org/v3/registration3/awscrt/${CURRENT_TAG_VERSION}.json" | grep -e ^HTTP | tail -1 | grep 200)
+AWSCRTHTTP_STATUS=$(curl -IsL https://api.nuget.org/v3/registration3/awscrt-http/${CURRENT_TAG_VERSION}.json" | grep -e ^HTTP | tail -1 | grep 200)
+if [ -n $AWSCRT_STATUS ] &&  [ -n $AWSCRTHTTP_STATUS ]; then
     echo "$CURRENT_TAG_VERSION is already in NuGet, cut a new release if you want to publish."
     exit 1
 fi
