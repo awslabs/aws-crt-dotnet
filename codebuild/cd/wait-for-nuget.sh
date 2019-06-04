dae5e5708c4cd5e15f1ee30ade44708351877aa8
Justin Boswell <boswej@amazon.com>
2019-06-04 14:58:18 -0700

    made wait-for-nuget actually work

--- codebuild/cd/wait-for-nuget.sh
+++ codebuild/cd/wait-for-nuget.sh
@@ -9,7 +9,7 @@ start=$(date +%s)
 
 while true; do
     now=$(date +%s)
-    delta=$(expr $now - $start)
+    delta=$(expr $now - $start || true)
 
     awscrt_status=$(curl -IsL https://api.nuget.org/v3/registration3/awscrt/${version}.json | grep -e ^HTTP | tail -1 | grep -c 200 || true) 
     awscrthttp_status=$(curl -IsL https://api.nuget.org/v3/registration3/awscrt-http/${version}.json | grep -e ^HTTP | tail -1 | grep -c 200 || true)
@@ -23,6 +23,8 @@ while true; do
         echo "Timed out waiting for packages to be available, giving up."
         exit 1
     fi
+
+    sleep 5
 done
 
 exit 0
