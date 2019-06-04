a377da8f357e38970e564c4dcef04486a75a5735
Justin Boswell <boswej@amazon.com>
2019-06-04 15:57:36 -0700

    make sure packages dir exists, windows edition

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,8 +1,8 @@
 
 call RefreshEnv.Cmd
 
-:: grab external dependencies, ignoring that they don't exist in the packages folder
-dotnet restore --ignore-failed-sources
+:: make sure packages dir exists
+md packages
 :: test will build and package, then run tests
 dotnet test --configuration Release -v normal %* || goto :error
 :: %TEMP%\depends\depends.exe /c /ot:%TEMP%\depends.log tests/bin/Release/netcoreapp2.1/aws-crt-dotnet.dll
