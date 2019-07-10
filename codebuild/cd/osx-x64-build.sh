97117be92d204973a20f1dae764796cd6b05a1e7
Justin Boswell <boswej@amazon.com>
2019-07-10 10:42:06 -0700

    install dotnet on osx before trying to use it

--- codebuild/cd/osx-x64-build.sh
+++ codebuild/cd/osx-x64-build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+if ! type -P dotnet &> /dev/null; then
+    cd /tmp
+    curl -LO https://dot.net/v1/dotnet-install.sh
+    chmod u+x dotnet-install.sh
+    ./dotnet-install.sh --channel 2.2
+fi
+
 dotnet build --configuration Release
 mkdir -p ../dist/x64
 cp -rv build/lib/libaws-crt-dotnet-x64.dylib ../dist/
