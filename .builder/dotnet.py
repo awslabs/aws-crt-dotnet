e5f1ef56690c6b63ce0a9170b7c79aa09512455f
Justin Boswell <boswej@amazon.com>
2020-04-28 09:18:39 -0700

    Removed unused mirror_package

--- .builder/dotnet.py
+++ .builder/dotnet.py
@@ -12,7 +12,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from fetch import fetch_script, mirror_package
+from fetch import fetch_script
 from project import Import
 
 import os
