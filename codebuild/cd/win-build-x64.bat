7c439419b6bb1ba1973817bee0114ee9de774d64
Bret Ambrose <bretambrose@gmail.com>
2021-06-06 14:20:04 -0700

    More experimentation

--- codebuild/cd/win-build-x64.bat
+++ codebuild/cd/win-build-x64.bat
@@ -1,7 +1,7 @@
 
 @setlocal enableextensions enabledelayedexpansion
 
-dotnet build -f netstandard2.0 --configuration Release -p:PlatformTarget=x64 || goto error
+dotnet build -f netstandard2.0 --configuration Release -p:PlatformTarget=x64 -p:CMakeGenerator64="Visual Studio 14 2015 Win64" -p:CMakeGenerator86="Visual Studio 14 2015" || goto error
 
 md ..\dist\x86
 for /R c:\build-aws-crt\lib %%F IN (*) do (
