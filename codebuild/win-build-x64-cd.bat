26116f91a29a5a1941e9476390659c05f6787527
Bret Ambrose <bretambrose@gmail.com>
2021-06-08 14:13:46 -0700

    Yet another way of invoking this

---
+++ codebuild/win-build-x64-cd.bat
@@ -0,0 +1,20 @@
+
+@setlocal enableextensions enabledelayedexpansion
+
+call "C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/Common7/Tools/VsDevCmd.bat"
+dotnet build -f netstandard2.0 --configuration Release -p:PlatformTarget=x64 || goto error
+
+md ..\dist\x64
+for /R c:\build-aws-crt\lib %%F IN (*) do (
+    if NOT "%%~xF" == ".ilk" (
+        copy %%F ..\dist\
+    )
+)
+
+@endlocal
+goto :EOF
+
+:error
+@endlocal
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
