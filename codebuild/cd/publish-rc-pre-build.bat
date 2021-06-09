5381603a939427b29b45d0650ab5f76b4cc4679a
Bret Ambrose <bretambrose@gmail.com>
2021-06-09 10:53:35 -0700

    correct copy destination directory

--- codebuild/cd/publish-rc-pre-build.bat
+++ codebuild/cd/publish-rc-pre-build.bat
@@ -1,12 +1,12 @@
 
 set PATH=%PATH%;"C:/Program Files/Git/usr/bin"
 md packages
-md "build/lib"
+md build
 cp -rv %CODEBUILD_SRC_DIR_linux_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
 cp -rv %CODEBUILD_SRC_DIR_win_x86%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
 cp -rv %CODEBUILD_SRC_DIR_win_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
 cp -rv %CODEBUILD_SRC_DIR_osx_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
-ls -alR %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/lib
+ls -alR %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build
 
 goto :EOF
 
