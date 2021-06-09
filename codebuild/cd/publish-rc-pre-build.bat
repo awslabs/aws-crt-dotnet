fdca897eee52b4449b3ac4ba090125fc4f17eba3
Bret Ambrose <bretambrose@gmail.com>
2021-06-09 10:16:03 -0700

    Bad osx copy source path

--- codebuild/cd/publish-rc-pre-build.bat
+++ codebuild/cd/publish-rc-pre-build.bat
@@ -5,7 +5,7 @@ md "build/lib"
 cp -rv %CODEBUILD_SRC_DIR_linux_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/lib/ || goto :error
 cp -rv %CODEBUILD_SRC_DIR_win_x86%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/lib/ || goto :error
 cp -rv %CODEBUILD_SRC_DIR_win_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/lib/ || goto :error
-cp -rv %CODEBUILD_SRC_DIR_osx_x64%/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/lib/ || goto :error
+cp -rv %CODEBUILD_SRC_DIR_osx_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/lib/ || goto :error
 ls -alR %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/lib
 
 goto :EOF
