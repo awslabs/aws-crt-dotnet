7f82a2d9834839f4ba83c8d4990eea484969ea14
Bret Ambrose <bretambrose@gmail.com>
2021-06-09 18:20:27 -0700

    Cd iteration5 (#48)
    * Use windows copy command

--- codebuild/cd/publish-rc-pre-build.bat
+++ codebuild/cd/publish-rc-pre-build.bat
@@ -2,11 +2,12 @@
 set PATH=%PATH%;"C:/Program Files/Git/usr/bin"
 md packages
 md build
-cp -rv %CODEBUILD_SRC_DIR_linux_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
-cp -rv %CODEBUILD_SRC_DIR_win_x86%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
-cp -rv %CODEBUILD_SRC_DIR_win_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
-cp -rv %CODEBUILD_SRC_DIR_osx_x64%/dist/* %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build/ || goto :error
-ls -alR %CODEBUILD_SRC_DIR%/aws-crt-dotnet/build
+
+xcopy /S /Y %CODEBUILD_SRC_DIR_linux_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
+xcopy /S /Y %CODEBUILD_SRC_DIR_win_x86%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
+xcopy /S /Y %CODEBUILD_SRC_DIR_win_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
+xcopy /S /Y %CODEBUILD_SRC_DIR_osx_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
+dir /S %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build
 
 goto :EOF
 
