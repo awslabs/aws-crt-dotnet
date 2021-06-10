3207e6d48f9b0d47684bd40466fb95996104e814
Bret Ambrose <bretambrose@gmail.com>
2021-06-09 17:13:49 -0700

    Turns out original query with a tweaked uri works for publish detection

--- codebuild/cd/wait-for-nuget.sh
+++ codebuild/cd/wait-for-nuget.sh
@@ -12,13 +12,13 @@ while true; do
     now=$(date +%s)
     delta=$(expr $now - $start || true)
 
-    awscrt_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt/${version}.json | grep -c ^Error) 
-    awscrthttp_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-http/${version}.json | grep -c ^Error)
-    awscrtauth_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-auth/${version}.json | grep -c ^Error)
-    awscrtcal_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-cal/${version}.json | grep -c ^Error)
-    awscrtchecksums_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-checksums/${version}.json | grep -c ^Error)
+    awscrt_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt/${version}.json | grep -e ^HTTP | tail -1 | grep -c 200 || true) 
+    awscrthttp_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-http/${version}.json | grep -e ^HTTP | tail -1 | grep -c 200 || true)
+    awscrtauth_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-auth/${version}.json | grep -e ^HTTP | tail -1 | grep -c 200 || true)
+    awscrtcal_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-cal/${version}.json | grep -e ^HTTP | tail -1 | grep -c 200 || true)
+    awscrtchecksums_status=$(curl -IsL https://api.nuget.org/v3/registration5-semver1/awscrt-checksums/${version}.json | grep -e ^HTTP | tail -1 | grep -c 200 || true)
     echo "t=${delta}s AWSCRT=${awscrt_status} AWSCRT-HTTP=${awscrthttp_status} AWSCRT-AUTH=${awscrtauth_status} AWSCRT-CAL=${awscrtcal_status} AWSCRT-CHECKSUMS=${awscrtchecksums_status}"
-    if [ $awscrt_status -eq 0 ] && [ $awscrthttp_status -eq 0 ] && [ $awscrtauth_status -eq 0 ] && [ $awscrtcal_status -eq 0 ] && [ $awscrtchecksums_status -eq 0 ]; then
+    if [ $awscrt_status -gt 0 ] && [ $awscrthttp_status -gt 0 ] && [ $awscrtauth_status -gt 0 ] && [ $awscrtcal_status -gt 0 ] && [ $awscrtchecksums_status -gt 0 ]; then
         echo "Package(s) with version $version found in NuGet."
         break
     fi
