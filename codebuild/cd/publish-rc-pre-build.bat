7f57aef47d06e1d475ce62ba8c15758af949fb50
Bret Ambrose <bretambrose@gmail.com>
2021-06-09 19:47:59 -0700

    Freakin Jenkins (#49)

--- codebuild/cd/publish-rc-pre-build.bat
+++ codebuild/cd/publish-rc-pre-build.bat
@@ -6,7 +6,7 @@ md build
 xcopy /S /Y %CODEBUILD_SRC_DIR_linux_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
 xcopy /S /Y %CODEBUILD_SRC_DIR_win_x86%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
 xcopy /S /Y %CODEBUILD_SRC_DIR_win_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
-xcopy /S /Y %CODEBUILD_SRC_DIR_osx_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
+xcopy /S /Y %CODEBUILD_SRC_DIR_osx_x64%\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
 dir /S %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build
 
 goto :EOF
