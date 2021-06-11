d175abd296c9f1b17a5ba89d5743b67f5423c5be
Bret Ambrose <bretambrose@gmail.com>
2021-06-11 13:52:53 -0700

    This seemed to work

--- codebuild/cd/pull-signing-secrets.sh
+++ codebuild/cd/pull-signing-secrets.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-echo "ECS Uri environment:"
-echo $AWS_CONTAINER_CREDENTIALS_FULL_URI
-echo $AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
-
 MSYS_NO_PATHCONV=1 aws --region us-west-2 sts assume-role --role-arn arn:aws:iam::582595803497:role/aws-common-runtime-secrets-role --role-session-name DotnetCrtCD --query Credentials > $TEMP/creds.txt
 
 # these will unset when the script ends
