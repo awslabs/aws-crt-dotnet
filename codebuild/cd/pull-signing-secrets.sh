857a83b29adcf83896d65ce458bb02df3fd8e349
Bret Ambrose <bretambrose@gmail.com>
2021-06-11 13:48:37 -0700

    Try to disable windows path conversion pre-cli

--- codebuild/cd/pull-signing-secrets.sh
+++ codebuild/cd/pull-signing-secrets.sh
@@ -6,7 +6,7 @@ echo "ECS Uri environment:"
 echo $AWS_CONTAINER_CREDENTIALS_FULL_URI
 echo $AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
 
-aws --region us-west-2 sts assume-role --role-arn arn:aws:iam::582595803497:role/aws-common-runtime-secrets-role --role-session-name DotnetCrtCD --query Credentials > $TEMP/creds.txt
+MSYS_NO_PATHCONV=1 aws --region us-west-2 sts assume-role --role-arn arn:aws:iam::582595803497:role/aws-common-runtime-secrets-role --role-session-name DotnetCrtCD --query Credentials > $TEMP/creds.txt
 
 # these will unset when the script ends
 export AWS_ACCESS_KEY_ID=$(cat ${TEMP}/creds.txt | sed -n 's/.*"AccessKeyId": "\(.*\)".*/\1/p')
