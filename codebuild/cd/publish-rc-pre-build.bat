d257e381f55d0e4b27f857783f25d15a8f1a11d2
Bret Ambrose <bretambrose@gmail.com>
2021-08-01 13:21:17 -0700

    First draft of arm64 CD

--- codebuild/cd/publish-rc-pre-build.bat
+++ codebuild/cd/publish-rc-pre-build.bat
@@ -7,6 +7,12 @@ xcopy /S /Y %CODEBUILD_SRC_DIR_linux_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dot
 xcopy /S /Y %CODEBUILD_SRC_DIR_win_x86%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
 xcopy /S /Y %CODEBUILD_SRC_DIR_win_x64%\dist\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
 xcopy /S /Y %CODEBUILD_SRC_DIR_osx_x64%\* %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build\ || goto :error
+
+git describe --tags | cut -f1 -d'-' | cut -f2 -dv > version.txt
+set /P PKG_VERSION=<version.txt
+
+aws s3 cp --recursive s3://aws-crt-dotnet-pipeline/v${PKG_VERSION}/lib %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build   
+
 dir /S %CODEBUILD_SRC_DIR%\aws-crt-dotnet\build
 
 goto :EOF
