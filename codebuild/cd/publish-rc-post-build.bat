16808feeeec73b8c322c027c1ef697159185ec46
Michael Graeb <graebm@amazon.com>
2022-05-17 11:31:54 -0700

    Revert "revertme: allow test pipeline to run again"
    This reverts commit df9cce30fb1d09103c17a852010e08cc33a85596.

--- codebuild/cd/publish-rc-post-build.bat
+++ codebuild/cd/publish-rc-post-build.bat
@@ -5,7 +5,7 @@ set PATH=%PATH%;"C:/Program Files/Git/usr/bin"
 git describe --tags | cut -f1 -d'-' | cut -f2 -dv > version.txt
 set /P PKG_VERSION=<version.txt
 
-bash %CODEBUILD_SRC_DIR%/aws-crt-dotnet/codebuild/cd/wait-for-nuget.sh %PKG_VERSION%-rc1 || goto error
+bash %CODEBUILD_SRC_DIR%/aws-crt-dotnet/codebuild/cd/wait-for-nuget.sh %PKG_VERSION%-rc || goto error
 
 goto :EOF
 
