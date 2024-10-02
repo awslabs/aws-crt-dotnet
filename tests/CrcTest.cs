68293f8c43e0faf63afb20339b708df61903b562
DmitriyMusatkin <musatkd@amazon.com>
2024-10-02 13:22:40 -0700

    yeah they do

--- tests/CrcTest.cs
+++ tests/CrcTest.cs
@@ -122,7 +122,7 @@ namespace tests
             for (int i = 0; i < 32; i++) {
                 res = Crc.crc64nvme(new byte[1], res);
             }
-            ulong expected = 0xCF3473434D4ECF3A;
+            ulong expected = 0xCF3473434D4ECF3B;
             Assert.Equal(expected, res);
         }
     }
