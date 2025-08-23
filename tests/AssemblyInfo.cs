d4e7c2cbe16d41a90125b7a2d944ef210e611871
Dengke Tang <dengket@amazon.com>
2025-08-22 23:00:47 -0700

    still need this

---
+++ tests/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+using Xunit;
+
+// Disable parallel test execution for the entire assembly
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
